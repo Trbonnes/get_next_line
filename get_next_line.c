33f5dcedb921d4c8dd4924cc039ff639630f1f7c
Tristan <Seph@Laptop-Tristan.localdomain>
2019-10-12 19:30:05 +0200

    changement de static sur save

--- get_next_line.c
+++ get_next_line.c
@@ -3,12 +3,16 @@
 int get_next_line(int fd, char **line)
 {
 	char			*lect;
-	static char		*buffer;
+	char			*buffer;
+	static char		*save;
 	int				nb_o;
 
 	lect = *line;
 	if (*buffer == '\0')
+	{
 		buffer = malloc(BUFFER_SIZE);
+		save = '\0';
+	}
 	if (buffer == NULL)
 		return (-1);
 	nb_o = read(fd, buffer, BUFFER_SIZE);
@@ -16,11 +20,24 @@ int get_next_line(int fd, char **line)
 		return (-1);
 	while (*buffer != '\n' || *buffer != '\0')
 	{
+		while (save != '\n' || save != '\0')
+		{
+			*lect = *save;
+			lect++;
+			save++;
+		}
+		if (save == '\n')
+			return (1);
 		*lect = *buffer;
 		lect++;
 		buffer++;
 	}
+	save = buffer;
 	if (nb_o == 0)
-		return(0);
-	return(1);
+	{
+		free(buffer);
+		free(save);
+		return (0);
+	}
+	return (1);
 }
